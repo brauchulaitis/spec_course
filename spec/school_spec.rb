d74b98c1445a7a40d8aadef2aa40fa78785c5489
Oskars Braucs <oskars.braucs@gmail.com>
2024-02-07 14:53:15 +0200

    Updated school spec

--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -1,5 +1,5 @@
 class School
-  attr_reader :name, :students
+  attr_accessor :name, :students
 
   def initialize(name, students = [])
     @name = name
@@ -8,13 +8,15 @@ class School
 end
 
 RSpec.describe School do
+  let(:school) { School.new('Beverly Hills High School') }
+
   it 'has a name' do
-    school = School.new('Beverly Hills High School')
     expect(school.name).to eq('Beverly Hills High School')
+    school.name = 'Harward University'
+    expect(school.name).to eq('Harward University')
   end
 
   it 'should start off with no students' do
-    school = School.new('Notre Dame High School')
     expect(school.students).to eq([])
   end
 end
