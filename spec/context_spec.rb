68479c35a0ae9584d6ea73e6acfebe642e403ef9
Oskars Braucs <oskars.braucs@gmail.com>
2024-03-19 11:04:38 +0200

    Context method and Nested describes

---
+++ spec/context_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe "#even? method" do
+  # it "Should return true if number is even"
+  # it "Should return false if number is odd"
+
+  # describe "with even number" do
+  #   it "should return true" do
+  #     expect(4.even?).to eq(true)
+  #   end
+  # end
+
+  # describe "with odd number" do
+  #   it "should return false" do
+  #     expect(5.even?).to eq(false)
+  #   end
+  # end
+
+  context "with even number" do
+    it "should return true" do
+      expect(4.even?).to eq(true)
+    end
+  end
+
+  context "with odd number" do
+    it "should return false" do
+      expect(5.even?).to eq(false)
+    end
+  end
+end
