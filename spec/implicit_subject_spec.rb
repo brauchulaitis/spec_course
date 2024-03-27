e7fee899bc7703bb7d1289b0aef8572b7486e241
Oskars Braucs <oskars.braucs@gmail.com>
2024-03-27 12:16:02 +0200

    Implicit Subject

---
+++ spec/implicit_subject_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe Hash do
+  it "should start off empty" do
+    puts subject
+    puts subject.class
+    expect(subject.length).to eq(0)
+  end
+
+  it "is isolated between examples" do
+    expect(subject.length).to eq(0)
+  end
+end
